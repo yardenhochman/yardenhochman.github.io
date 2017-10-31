574c149a6d8085c3906c1d41d53bd6889acf3c50
yarden hochman <yyyarden@gmail.com>
2017-10-31 16:31:11 -0400

    added projects 2,3

--- src/App.js
+++ src/App.js
@@ -16,15 +16,35 @@ const App = () => {
       picture:"https://s1.postimg.org/6l5nmmgqv3/online_Page_Setup.jpg",
       demo:"https://sitegenerator-front.herokuapp.com/welcome/setup",
       codeUrl:"https://github.com/yardenhochman/create_your_page",
-      contex:"A quick setup process that allows visitors to easily establish online presence"
+      contex:"A quick setup process that allows visitors to easily establish online presence. Utilizes an API for image storage, and React for a fluid setup process."
+    },
+    {
+      title: "SoundLand (In Development Collaborative)",
+      picture: "https://d30y9cdsu7xlg0.cloudfront.net/png/1041163-200.png",
+      demo: "https://mighty-fjord-94542.herokuapp.com/",
+      codeUrl: "https://github.com/yardenhochman/GenreLand",
+      contex: "A platform for understanding musical tastes in relation to location, and connecting people with similar tastes. This website utilizes API calls to retrieve neighboring zipcodes & bar venues, and Database storage to create and manage meetups between users."
+    },
+    {
+      title: "Visual Weather (In Development)",
+      picture: "http://oi66.tinypic.com/2z7ifqr.jpg",
+      demo: "https://weather1989.herokuapp.com/",
+      codeUrl: "https://github.com/yardenhochman/Visual-Weather",
+      contex: "A weather app with automatic location detection and API for local weather forecast. Aims to represent the forecast visually, no text"
     },
     {
       title:"Simon Says",
       picture:"http://i67.tinypic.com/35344gk.jpg",
-      demo:"https://yardenhochman.github.io/projectOne",
-      codeUrl:"https://github.com/yardenhochman/projectOne",
+      demo:"https://yardenhochman.github.io/Simon-Says",
+      codeUrl:"https://github.com/yardenhochman/Simon-Says",
       contex:"A memory game written based entirely off constructors, all in ECMAScript2015 standard code."
     },
+  
+
+
+
+
+    https://visualweatheryarden.herokuapp.com/
     /* {
       title:"Weather Sense",
       picture:"",
