d3316a9b8e9cdca6046717120fcfd2c0138c5f45
Yarden Hochman <yyyarden@gmail.com>
2017-12-29 12:09:16 -0500

    added link to demo

--- src/App.js
+++ src/App.js
@@ -1,48 +1,63 @@
 import React from 'react';
 import './App.css';
-import Intro from './components/intro'
-import Projects from './components/projects'
-import Bio from './components/bio'
+import Intro from './components/intro';
+import Projects from './components/projects';
+import Bio from './components/bio';
 const App = () => {
-   
   const intro = {
-                  name:"Yarden Hochman",email:"yyyarden@gmail.com",
-                  occupation:"Full Stack Web Developer",
-                  sm:{instagram:"yardenhochman",twitter:"YardenH2",linkedIn:"yardenhochman",github:"yardenhochman"}
+    name: 'Yarden Hochman',
+    email: 'yyyarden@gmail.com',
+    occupation: 'Full Stack Web Developer',
+    sm: {
+      instagram: 'yardenhochman',
+      twitter: 'YardenH2',
+      linkedIn: 'yardenhochman',
+      github: 'yardenhochman'
     }
+  };
   const projects = [
     {
-      title:"Your Online Page",
-      picture:"https://s1.postimg.org/6l5nmmgqv3/online_Page_Setup.jpg",
-      demo:"https://sitegenerator-front.herokuapp.com/welcome/setup",
-      codeUrl:"https://github.com/yardenhochman/create_your_page",
-      contex:"A quick setup process that allows visitors to easily establish online presence. Utilizes an API for image storage, and React for a fluid setup process."
+      title: 'Your Online Page',
+      picture:
+        'https://s1.postimg.org/6l5nmmgqv3/online_Page_Setup.jpg',
+      demo:
+        'https://yourpage.herokuapp.com/main/setup',
+      codeUrl:
+        'https://github.com/Yardel1/create_your_page',
+      contex:
+        'A quick setup process that allows visitors to easily establish online presence. Utilizes an API for image storage, and React for a fluid setup process.'
     },
     {
-      title: "SoundLand",
-      picture: "https://d30y9cdsu7xlg0.cloudfront.net/png/1041163-200.png",
-      demo: "https://mighty-fjord-94542.herokuapp.com/",
-      codeUrl: "https://github.com/yardenhochman/GenreLand",
-      contex: "A Collaborative project, intended to research the relationship between musical tastes and location, and facilitate connections between people with similar musical tastes. (In Development)"
+      title: 'SoundLand',
+      picture:
+        'https://d30y9cdsu7xlg0.cloudfront.net/png/1041163-200.png',
+      demo: 'https://genremap.herokuapp.com/',
+      codeUrl:
+        'https://github.com/yardenhochman/GenreLand',
+      contex:
+        'A Collaborative project, intended to research the relationship between musical tastes and location, and facilitate connections between people with similar musical tastes.'
     },
+    /* {
+      title: 'Visual Weather',
+      picture:
+        'http://oi66.tinypic.com/2z7ifqr.jpg',
+      demo: 'https://weather1989.herokuapp.com/',
+      codeUrl:
+        'https://github.com/yardenhochman/Visual-Weather',
+      contex:
+        'A weather app with automatic location detection and API for local weather forecast. Aims to represent the forecast visually, no text.'
+    }, */
     {
-      title: "Visual Weather",
-      picture: "http://oi66.tinypic.com/2z7ifqr.jpg",
-      demo: "https://weather1989.herokuapp.com/",
-      codeUrl: "https://github.com/yardenhochman/Visual-Weather",
-      contex: "A weather app with automatic location detection and API for local weather forecast. Aims to represent the forecast visually, no text. (In Development)"
-    },
-    {
-      title:"Simon Says",
-      picture:"http://i67.tinypic.com/35344gk.jpg",
-      demo:"https://yardenhochman.github.io/Simon-Says",
-      codeUrl:"https://github.com/yardenhochman/Simon-Says",
-      contex:"A memory game written based entirely off constructors, all in ECMAScript2015 standard code."
-    },
-  
-
-
-
+      title: 'Simon Says',
+      picture:
+        'http://i67.tinypic.com/35344gk.jpg',
+      demo:
+        'https://yardenhochman.github.io/Simon-Says',
+      codeUrl:
+        'https://github.com/yardenhochman/Simon-Says',
+      contex:
+        'A memory game written based entirely off constructors, all in ECMAScript2015 standard code.'
+    }
 
     /* {
       title:"Weather Sense",
@@ -58,21 +73,24 @@ const App = () => {
       codeUrl:"https://github.com/yardenhochman/GenreLand",
       contex:"A platform for understanding musical taste in relation to location"
     }, */
-  ]
-  const vc = { VCFile:{link:"./file/Yarden Hochman CV.pdf"} }
-  const bio = 
-  { 
-    myPicUrl:"http://alturl.com/ft42i", 
-    description:"A multi-disciplinary programmer with full stack web development experience, looking for opportunities to develop engaging websites for products people need. I’m an idealist autodidact who loves to experiment and uncover the optimal path for each challenge I face.",
-  }
-    return (
-        <main>
-          <Intro intro={intro} />
-          <Projects projects={projects} />
-          <Bio bio={bio} vc={vc}/>
-        </main>
-    );
-  }
-
+  ];
+  const vc = {
+    VCFile: {
+      link: './file/Yarden Hochman CV.pdf'
+    }
+  };
+  const bio = {
+    myPicUrl: 'http://alturl.com/ft42i',
+    description:
+      'A multi-disciplinary programmer with full stack web development experience, looking for opportunities to develop engaging websites for products people need. I’m an idealist autodidact who loves to experiment and uncover the optimal path for each challenge I face.'
+  };
+  return (
+    <main>
+      <Intro intro={intro} />
+      <Projects projects={projects} />
+      <Bio bio={bio} vc={vc} />
+    </main>
+  );
+};
 
 export default App;
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,50 +1,76 @@
 import React, { Component } from 'react';
 
-
 class Projects extends Component {
-    constructor() {
-        super();
-        this.state = {project: 0}
-    }
+  state = { project: 0 };
 
-    setActiveProject(e) {
-        this.setState({project: e.target.name})
-    }
+  setActiveProject = e => {
+    this.setState({ project: e.target.name });
+  };
 
-    listProjects(projects) {
-        return projects.map( (project,i) => (
-            <div className="project_block" key={project.codeUrl}>
-                <a href={project.codeUrl} >
-                    <img className={`project_image image_${project.title}`} name={i+1} src={project.picture} onMouseOver={e => this.setActiveProject(e)}/>
-                </a>
-            </div>
-        ))
-    }
-    render() {
-        let active = this.state.project
-        const {projects} = this.props
-        return (
-            <section className="container-fluid">
-                <a name="projects"/>
-                <div className="project_title_space">
-                    {active?<p className="project_title">{projects[active-1].title}</p>:""}
-                </div>
-                <div className="project_images">
-                    {this.listProjects(projects)}
-                </div>
-                <div className="project_description_space">
-                    {active?<p className="project_description">{projects[active-1].contex}</p>:""}
-                </div>
-
-            </section>
-        )
-    }
-
-
-} 
+  listProjects = projects => {
+    return projects.map((project, i) => (
+      <div
+        className="project_block"
+        key={project.codeUrl}
+      >
+        <a href={project.codeUrl}>
+          <img
+            className={`project_image image_${
+              project.title
+            }`}
+            name={i + 1}
+            src={project.picture}
+            onMouseOver={e =>
+              this.setActiveProject(e)
+            }
+          />
+        </a>
+      </div>
+    ));
+  };
+  render() {
+    let active = this.state.project;
+    const { projects } = this.props;
+    return (
+      <section className="container-fluid">
+        <a name="projects" />
+        <div className="project_title_space">
+          {active ? (
+            <p className="project_title">
+              {projects[active - 1].title}
+            </p>
+          ) : (
+            ''
+          )}
+        </div>
+        <div className="project_images">
+          {this.listProjects(projects)}
+        </div>
+        <div className="project_description_space">
+          {active ? (
+            <p className="project_description">
+              {projects[active - 1].contex}
+            </p>
+          ) : (
+            ''
+          )}
+          {active ? (
+            <a
+              href={projects[active - 1].demo}
+              className="project_link"
+            >
+              Demo
+            </a>
+          ) : (
+            ''
+          )}
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Projects;
-
 
 /* 
 Todo:
